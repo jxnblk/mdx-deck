3350ec933a8faabca8a43d550765d72c6e7921a4
Prashant Andani <prashant.andani@gmail.com>
2018-07-30 21:45:44 +0530

    Remove unused imports and Add prettierrc
    Reindented, removed unused imports added prettier for auto code format

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import SlideDeck from './index'
+import React from "react"
+import SlideDeck from "./index"
 
 const mod = require(DOC_FILENAME)
 const slides = mod.default
@@ -9,14 +8,10 @@ const { theme, components, Provider } = mod
 const Root = Provider || React.Fragment
 
 export default class App extends React.Component {
-  render () {
+  render() {
     return (
       <Root>
-        <SlideDeck
-          slides={slides}
-          theme={theme}
-          components={components}
-        />
+        <SlideDeck slides={slides} theme={theme} components={components} />
       </Root>
     )
   }
