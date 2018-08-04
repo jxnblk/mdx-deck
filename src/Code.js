5c936c49e0bc18bb7f3176cb199fb9c4d48d27f5
busypeoples <busypeoples@gmail.com>
2018-08-04 12:56:19 +0200

    Clean up

--- src/Code.js
+++ src/Code.js
@@ -4,11 +4,9 @@ import { withTheme } from 'styled-components'
 import SyntaxHighlighter, { registerLanguage } from "react-syntax-highlighter/prism-light"
 import defaultTheme from 'react-syntax-highlighter/styles/prism/atom-dark'
 import javascript from 'react-syntax-highlighter/languages/prism/javascript'
-import php from 'react-syntax-highlighter/languages/prism/php'
 import elixir from 'react-syntax-highlighter/languages/prism/elixir'
 
 registerLanguage('javascript', javascript);
-// registerLanguage('php', php);
 registerLanguage('elixir', elixir);
 
 export default withTheme(class Code extends React.Component {
