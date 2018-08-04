1fca35ad73cbe8389c928f66b32c844c68b950ec
Brent Jackson <jxnblk@gmail.com>
2018-08-04 17:09:10 -0400

    Adjust overview mode styles

--- src/Overview.js
+++ src/Overview.js
@@ -9,36 +9,32 @@ export default ({
   slides = [],
   update
 }) => (
-  <Flex
-    bg='black'
-    p={3}
-    css={{
-      justifyContent: 'flex-start',
-      flexWrap: 'wrap'
-    }}>
-    {slides.map((Component, i) => (
-      <Box key={i}
-        m={2}
-        css={{
-          border: '1px solid rgba(128, 128, 128, 0.25)'
-        }}>
-        <div role='link'
-          href={'#' + i}
-          onClick={e => {
-            update({
-              index: i,
-              mode: 'NORMAL'
-            })
-          }}>
-          <Zoom zoom={1/4}>
-            <Root width='100vw' height='100vh'>
-              <Slide index={i}>
-                <Component />
-              </Slide>
-            </Root>
-          </Zoom>
-        </div>
-      </Box>
-    ))}
-  </Flex>
+  <Box bg='black' css={{ minHeight: '100vh' }}>
+    <Flex
+      css={{
+        justifyContent: 'flex-start',
+        flexWrap: 'wrap'
+      }}>
+      {slides.map((Component, i) => (
+        <Box key={i}>
+          <div role='link'
+            href={'#' + i}
+            onClick={e => {
+              update({
+                index: i,
+                mode: 'NORMAL'
+              })
+            }}>
+            <Zoom zoom={1/4}>
+              <Root width='100vw' height='100vh'>
+                <Slide index={i}>
+                  <Component />
+                </Slide>
+              </Root>
+            </Zoom>
+          </div>
+        </Box>
+      ))}
+    </Flex>
+  </Box>
 )
