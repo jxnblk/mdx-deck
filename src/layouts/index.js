36b8a74e40c5cc9f8d99da5394c1d5c2a07dd5ef
Brent Jackson <jxnblk@gmail.com>
2018-08-06 19:40:28 -0400

    Add FullScreenCode layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,4 @@
 export { default as Invert } from './Invert'
 export { default as Split } from './Split'
 export { default as SplitRight } from './SplitRight'
+export { default as FullScreenCode } from './FullScreenCode'
