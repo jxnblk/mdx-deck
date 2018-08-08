3e98e35a8dae042c1d15c237752c7f471b1f2eca
Michael Hopper <drmikeh@gmail.com>
2018-08-08 12:14:54 -0400

    Fixes missing Dots and navigation buttons on initial load.

--- src/index.js
+++ src/index.js
@@ -151,6 +151,7 @@ export class SlideDeck extends React.Component {
 
   getMode = () => {
     const { mode } = querystring.parse(window.location.search.replace(/^\?/, ''))
+    mode = mode || 'normal'
     this.setState({
       mode: modes[mode]
     })
