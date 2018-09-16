e062ae2ab482bd77bb5d38236d520b0f9516fee3
Sara Vieira <hey@iamsaravieira.com>
2018-09-16 18:57:36 +0200

    adds button to go back to normal mode in presenter

--- src/Timer.js
+++ src/Timer.js
@@ -1,33 +1,9 @@
 import React from 'react'
 import hhmmss from 'hhmmss'
-import styled from 'styled-components'
-import { space, color } from 'styled-system'
 import Flex from './Flex'
 import Box from './Box'
 import Mono from './Mono'
-
-const Button = styled.button([], {
-  appearance: 'none',
-  fontFamily: 'inherit',
-  fontSize: '12px',
-  fontWeight: 'bold',
-  borderRadius: '4px',
-  border: 'none'
-}, space, color)
-
-Button.propTypes = {
-  ...space.propTypes,
-  ...color.propTypes
-}
-
-Button.defaultProps = {
-  m: 0,
-  px: 2,
-  py: 1,
-  color: 'white',
-  bg: '#333'
-}
-
+import Button from './Button'
 class Timer extends React.Component {
   state = {
     on: false,
@@ -47,43 +23,36 @@ class Timer extends React.Component {
     const now = new Date()
     this.setState(state => ({
       time: now.toLocaleTimeString(),
-      seconds: state.on
-        ? state.seconds + 1
-        : state.seconds
+      seconds: state.on ? state.seconds + 1 : state.seconds
     }))
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.timer = setInterval(this.tick, 1000)
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     if (!this.timer) return
     clearInterval(this.timer)
   }
 
-  render () {
+  render() {
     const { time, seconds, on } = this.state
     const elapsed = hhmmss(seconds)
 
     return (
       <Flex css={{ alignItems: 'center' }}>
-        {!on && seconds > 0 && (
-          <Button mr={1} onClick={this.reset}>
-            reset
-          </Button>
-        )}
-        <Button
-          bg={on ? '#600' : '#060'}
-          onClick={this.toggle}>
+        {!on &&
+          seconds > 0 && (
+            <Button mr={1} onClick={this.reset}>
+              reset
+            </Button>
+          )}
+        <Button bg={on ? '#600' : '#060'} onClick={this.toggle}>
           {on ? 'stop' : 'start'}
         </Button>
-        <Mono px={2}>
-          {elapsed} |
-        </Mono>
-        <Mono>
-          {time}
-        </Mono>
+        <Mono px={2}>{elapsed} |</Mono>
+        <Mono>{time}</Mono>
       </Flex>
     )
   }
