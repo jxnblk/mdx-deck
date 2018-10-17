5a80de385da897ce914e66cb234ced781a3e38fb
Albino Tonnina <albinotonnina@gmail.com>
2018-10-17 23:49:06 +0100

    Appear with alt prop

--- src/Appear.js
+++ src/Appear.js
@@ -4,42 +4,48 @@ import { withDeck } from './context'
 import { setSteps } from './updaters'
 import { modes } from './constants'
 
-export default withDeck(class Appear extends React.Component {
-  static propTypes = {
-    children: PropTypes.array.isRequired,
-    deck: PropTypes.object.isRequired
-  }
-
-  constructor (props) {
-    super(props)
-    const { update, index } = props.deck
-    const steps = React.Children.toArray(props.children).length
-    update(setSteps(index, steps))
-  }
-
-  render() {
-    const children = React.Children.toArray(this.props.children)
-      .map(child => typeof child === 'string'
-        ? <div>{child}</div>
-        : child
-      )
-    const { step, mode } = this.props.deck
-
-    if (mode === modes.grid) {
-      return children
+export default withDeck(
+  class Appear extends React.Component {
+    static propTypes = {
+      children: PropTypes.array.isRequired,
+      alt: PropTypes.bool,
+      deck: PropTypes.object.isRequired,
     }
 
-    return (
-      <React.Fragment>
-        {children.map((child, i) => (
-          React.cloneElement(child, {
-            key: i,
-            style: {
-              visibility: (step >= i + 1) ? 'visible' : 'hidden'
-            }
-          })
-        ))}
-      </React.Fragment>
-    )
+    constructor(props) {
+      super(props)
+      const { update, index } = props.deck
+      const steps = React.Children.toArray(props.children).length
+      update(setSteps(index, steps))
+    }
+
+    render() {
+      const children = React.Children.toArray(this.props.children).map(
+        child => (typeof child === 'string' ? <div>{child}</div> : child)
+      )
+      const {
+        alt,
+        deck: { step, mode },
+      } = this.props
+
+      if (mode === modes.grid) {
+        return children
+      }
+
+      const onlyCurrentChild = (_, i) => !alt || (alt && i === step - 1)
+
+      return (
+        <React.Fragment>
+          {children.filter(onlyCurrentChild).map((child, i) =>
+            React.cloneElement(child, {
+              key: i,
+              style: {
+                visibility: step >= i + 1 ? 'visible' : 'hidden',
+              },
+            })
+          )}
+        </React.Fragment>
+      )
+    }
   }
-})
+)
