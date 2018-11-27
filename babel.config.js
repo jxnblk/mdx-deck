2bcf7d3e5ae6eb2252600a0bdedbff5225069f34
Brent Jackson <jxnblk@gmail.com>
2018-11-27 03:13:43 +0000

    Update babel

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-syntax-dynamic-import',
+    'babel-plugin-styled-components',
+  ],
+}
