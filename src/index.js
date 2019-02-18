5e220b76f1377fed2ac3b231f13ae4e6a1b0bbec
Brent Jackson <jxnblk@gmail.com>
2019-02-18 16:16:55 -0500

    Add Step render props component

--- src/index.js
+++ src/index.js
@@ -567,3 +567,19 @@ export const Appear = withContext(
     }
   }
 )
+
+export const Steps = withContext(
+  class extends React.Component {
+    constructor(props) {
+      super(props)
+      const { register, index } = props.context
+      const { length } = props
+      register(index, { steps: length })
+    }
+    render() {
+      const { context, render } = this.props
+      const { step } = context
+      return render({ step })
+    }
+  }
+)
