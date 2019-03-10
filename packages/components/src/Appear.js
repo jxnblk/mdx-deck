2798bfadafc691edb70f2a3a147cd41e3124757a
Brent Jackson <jxnblk@gmail.com>
2019-03-09 23:11:49 -0500

    Remove unused code

--- packages/components/src/Appear.js
+++ packages/components/src/Appear.js
@@ -1,28 +1,5 @@
 import React from 'react'
-import Steps from './Steps'
 import useSteps from './useSteps'
-
-export const _Appear = props => {
-  const arr = React.Children.toArray(props.children)
-  return (
-    <Steps
-      length={arr.length}
-      render={({ step }) => {
-        const children = arr.map((child, i) =>
-          i < step
-            ? child
-            : React.cloneElement(child, {
-                style: {
-                  ...child.props.style,
-                  visibility: 'hidden',
-                },
-              })
-        )
-        return <>{children}</>
-      }}
-    />
-  )
-}
 
 export const Appear = props => {
   const arr = React.Children.toArray(props.children)
