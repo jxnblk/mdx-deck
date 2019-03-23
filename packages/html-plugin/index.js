25fe122b9659d9e8a8362725fc3cb62d14d270ed
Brent Jackson <jxnblk@gmail.com>
2019-03-23 14:30:11 -0400

    Use mini-css-extract-plugin

--- packages/html-plugin/index.js
+++ packages/html-plugin/index.js
@@ -73,7 +73,7 @@ const defaultTemplate = ({
     <meta name='viewport' content='width=device-width,initial-scale=1'>
     <style>*{box-sizing:border-box}body{margin:0;font-family:system-ui,sans-serif}</style>
     <meta name='generator' content='mdx-deck'>
-    ${head}${css}${links}
+    ${head}${links}
   </head>
   <body>
     <div id=root>${body}</div>
