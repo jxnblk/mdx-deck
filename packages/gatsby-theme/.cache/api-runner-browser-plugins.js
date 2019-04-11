d79aa95bb19f64ebf35ab7fbc09de6bde15fb114
Brent Jackson <jxnblk@gmail.com>
2019-04-10 20:22:40 -0400

    Clean up

--- packages/gatsby-theme/.cache/api-runner-browser-plugins.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  {
-    plugin: require('/Users/jxnblk/repos/mdx-deck/node_modules/gatsby-mdx/gatsby-browser.js'),
-    options: { plugins: [], extensions: ['.mdx', '.md'] },
-  },
-]
