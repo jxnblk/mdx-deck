6e69a0e5af46755cc5bc40c2d8d9f385322097d2
Brent Jackson <jxnblk@gmail.com>
2019-04-12 18:36:13 -0400

    Refactor Slide component

--- packages/components/src/Slide.js
+++ packages/components/src/Slide.js
@@ -1,27 +1,21 @@
 import React from 'react'
-import styled from '@emotion/styled'
 import Root from './Root'
 import { Context } from './context'
 
-const SlideRoot = styled.div(
-  {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    overflow: 'hidden',
-    width: '100%',
-    height: '100%',
-  },
-  props => props.theme.Slide
-)
-
-export const Slide = ({ children, style, ...props }) => (
-  <Context.Provider value={props}>
-    <Root style={style}>
-      <SlideRoot>{children}</SlideRoot>
-    </Root>
+export const Slide = ({ index, context, ...props }) => (
+  <Context.Provider
+    value={{
+      index,
+      ...context,
+      ...props,
+    }}
+  >
+    <Root {...props} />
   </Context.Provider>
 )
+
+Slide.defaultProps = {
+  step: Infinity,
+}
 
 export default Slide
