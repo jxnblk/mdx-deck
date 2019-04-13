eff07b3e362d5869d59d779a70ebe6a8ebef6b6e
Brent Jackson <jxnblk@gmail.com>
2019-04-13 12:15:47 -0400

    Move styles into Slide component

--- packages/components/src/Slide.js
+++ packages/components/src/Slide.js
@@ -1,6 +1,72 @@
 import React from 'react'
-import Root from './Root'
+import styled from '@emotion/styled'
 import { Context } from './context'
+
+const themed = (...tags) => props =>
+  tags.map(tag => props.theme[tag] && { ['& ' + tag]: props.theme[tag] })
+
+const themedHeadings = props => ({
+  '& h1, & h2, & h3, & h4, & h5, & h6': props.theme.heading,
+})
+
+const themedLinks = props => ({
+  '& a': {
+    color: props.theme.colors.link,
+  },
+})
+
+// backwards compatibility
+const themedQuote = props => ({
+  '& blockquote': props.theme.quote,
+})
+
+const themedCode = props => ({
+  '& code, & pre': {
+    fontFamily: props.theme.monospace,
+    color: props.theme.colors.code,
+    background: props.theme.colors.codeBackground,
+  },
+})
+
+export const Root = styled.div(
+  props => ({
+    fontFamily: props.theme.font,
+    color: props.theme.colors.text,
+    backgroundColor: props.theme.colors.background,
+    // testing
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  }),
+  props => props.theme.css,
+  props => props.theme.Slide,
+  themedLinks,
+  themedHeadings,
+  themedCode,
+  themedQuote,
+  themed(
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'a',
+    'ul',
+    'ol',
+    'li',
+    'p',
+    'blockquote',
+    'img',
+    'table',
+    'pre',
+    'code'
+  )
+)
 
 export const Slide = ({ index, context, ...props }) => (
   <Context.Provider
