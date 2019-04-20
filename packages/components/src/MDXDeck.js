6c435ddf91f6cfd97ebb4231e695018fad7f658d
Brent Jackson <jxnblk@gmail.com>
2019-04-20 11:21:17 -0400

    Refactor search params

--- packages/components/src/MDXDeck.js
+++ packages/components/src/MDXDeck.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { Router, globalHistory, navigate } from '@reach/router'
 import { Global } from '@emotion/core'
 import { Swipeable } from 'react-swipeable'
-import querystring from 'querystring'
 import Provider from './Provider'
 import Slide from './Slide'
 import Presenter from './Presenter'
@@ -12,6 +11,7 @@ import Grid from './Grid'
 import Print from './Print'
 import GoogleFonts from './GoogleFonts'
 import Catch from './Catch'
+import QueryString from './QueryString'
 
 const NORMAL = 'normal'
 const PRESENTER = 'presenter'
@@ -190,17 +190,9 @@ export class MDXDeck extends React.Component {
     }
   }
 
-  getMode = () => {
-    const query = querystring.parse(
-      globalHistory.location.search.replace(/^\?/, '')
-    )
-    this.setState(query)
-  }
-
   componentDidMount() {
     document.body.addEventListener('keydown', this.handleKeyDown)
     window.addEventListener('storage', this.handleStorageChange)
-    this.getMode()
   }
 
   componentWillUnmount() {
@@ -210,19 +202,9 @@ export class MDXDeck extends React.Component {
 
   componentDidUpdate() {
     const index = this.getIndex()
-    const { step, mode } = this.state
-    const { pathname, search } = globalHistory.location
+    const { step } = this.state
     localStorage.setItem(STORAGE_INDEX, index)
     localStorage.setItem(STORAGE_STEP, step)
-
-    if (mode !== NORMAL && mode !== PRINT) {
-      const query = '?' + querystring.stringify({ mode })
-      if (query === search) return
-      navigate(query)
-    } else {
-      if (!search) return
-      navigate(pathname)
-    }
   }
 
   componentDidCatch(err) {
@@ -274,6 +256,7 @@ export class MDXDeck extends React.Component {
       <Provider {...this.props} {...this.state} mode={mode} index={index}>
         {style}
         <Catch>
+          <QueryString {...this.state} modes={modes} index={index} />
           <GoogleFonts />
           <Wrapper {...this.props} {...this.state} modes={modes} index={index}>
             <Swipeable onSwipedRight={this.previous} onSwipedLeft={this.next}>
