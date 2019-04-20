6c2efe83d22a093f248385dc63f1f16c42b16e2d
Brent Jackson <jxnblk@gmail.com>
2019-04-20 16:30:31 -0400

    Adjust styles for print

--- packages/components/src/Style.js
+++ packages/components/src/Style.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { globalHistory } from '@reach/router'
 import { Global } from '@emotion/core'
+
+const isPrintPath = () => {
+  const { pathname } = globalHistory.location
+  const parts = pathname.split('/')
+  const path = parts[parts.length - 1]
+  return path === 'print'
+}
 
 export default ({ mode, modes }) => {
   if (mode === modes.PRINT) return false
+  if (isPrintPath()) return false
   return (
     <Global
       styles={{
