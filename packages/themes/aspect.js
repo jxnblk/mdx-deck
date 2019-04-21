5e5fcd4e4e6a4e9e2ad0f93069fd660260346a46
Brent Jackson <jxnblk@gmail.com>
2019-04-21 17:41:00 -0400

    Adjust base aspect theme

--- packages/themes/aspect.js
+++ packages/themes/aspect.js
@@ -11,15 +11,6 @@ export default theme => {
     aspectRatio: 9 / 16,
     css: {
       fontSize: '1em',
-      '@media screen and (min-width:56em)': {
-        fontSize: 'inherit',
-      },
-      '@media screen and (min-width:64em)': {
-        fontSize: 'inherit',
-      },
-      '@media print': {
-        fontSize: 'inherit',
-      },
     },
   })
 }
