e561094631e0cba0d39895cbf4263f4060e72284
anotherjsguy <kuldeepkeshwar@gmail.com>
2019-06-05 19:07:08 +0530

    Update Socket.js

--- packages/components/src/Socket.js
+++ packages/components/src/Socket.js
@@ -1,73 +1,81 @@
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-const STORAGE_INDEX = 'mdx-slide'
-const STORAGE_STEP = 'mdx-step'
-
-const socket = new WebSocket('ws://localhost:8080');
-socket.addEventListener('open', function () {
-  console.log('ws:connected !!')
+const STORAGE_INDEX = "mdx-slide";
+const STORAGE_STEP = "mdx-step";
+const url= window.location.origin.replace('https:','wss:').replace('http:','ws:')
+const socket = new WebSocket(`${url}/__socket`);
+socket.addEventListener("open", function() {
+  console.log("ws:connected !!");
 });
-socket.addEventListener('error', function (event) {
-  console.log('ws:error !!',event)
+socket.addEventListener("error", function(event) {
+  console.log("ws:error !!", event);
 });
-
-
-function useSocketMessage({ goto, update,index, step }){
-  useEffect(function(){
-    function handler(event) {
-      const data=JSON.parse(event.data);
-      console.log('Message from server ', data);
-      if(data.type==='broadcast'){
-        const value=data.payload.value-0;
-        if(data.payload.type===STORAGE_INDEX && value!==index){
-          localStorage.setItem(STORAGE_INDEX, value)
-          goto(value)
-        }else if(data.payload.type===STORAGE_STEP && value!==step){
-          localStorage.setItem(STORAGE_STEP, value)
-          update({ step: value})
+function useSocketMessage({ goto, update, index, step }) {
+  useEffect(
+    function() {
+      function handler(event) {
+        const data = JSON.parse(event.data);
+        console.log("Message from server ", data);
+        if (data.type === "broadcast") {
+          const value = data.payload.value - 0;
+          if (data.payload.type === STORAGE_INDEX && value !== index) {
+            localStorage.setItem(STORAGE_INDEX, value);
+            goto(value);
+          } else if (data.payload.type === STORAGE_STEP && value !== step) {
+            localStorage.setItem(STORAGE_STEP, value);
+            update({ step: value });
+          }
         }
-      }else{
-        console.log('Message from server ', data);
       }
-    }
-    socket.addEventListener('message', handler);
-    return function(){
-      socket.removeEventListener('message', handler);
-    }
-  },[goto, update])
+      socket.addEventListener("message", handler);
+      return function() {
+        socket.removeEventListener("message", handler);
+      };
+    },
+    [goto, update]
+  );
 }
 const useSocket = (slide, step) => {
   useEffect(() => {
     try {
-        const msg=JSON.stringify({type:'broadcast',payload:{
-          type:STORAGE_INDEX,
-          value:slide,
-          [STORAGE_STEP]:step
-        }})
+      if (document.hasFocus()) {
+        const msg = JSON.stringify({
+          type: "broadcast",
+          payload: {
+            type: STORAGE_INDEX,
+            value: slide,
+            [STORAGE_STEP]: step
+          }
+        });
         socket.send(msg);
-      } catch (err) {
-        console.error('error while sending ws:message ',err)
       }
-  }, [slide])
+    } catch (err) {
+      console.error("error while sending ws:message ", err);
+    }
+  }, [slide]);
   useEffect(() => {
     try {
-        const msg=JSON.stringify({type:'broadcast',payload:{
-          type:STORAGE_STEP,
-          value:step,
-          [STORAGE_INDEX]:slide,
-        }})
+      if (document.hasFocus()) {
+        const msg = JSON.stringify({
+          type: "broadcast",
+          payload: {
+            type: STORAGE_STEP,
+            value: step,
+            [STORAGE_INDEX]: slide
+          }
+        });
         socket.send(msg);
-      } catch (err) {
-        console.error('error while sending ws:message ',err)
       }
-  }, [step])
-}
-
-
-
+    } catch (err) {
+      console.error("error while sending ws:message ", err);
+    }
+  }, [step]);
+};
 
 export default ({ goto, update, index, step }) => {
-  useSocketMessage({ goto, update ,index, step});
-  useSocket(index,step)
-  return false
-}
+  if(process.env.NODE_ENV==='development'){
+    useSocketMessage({ goto, update, index, step });
+    useSocket(index, step);
+  }
+  return false;
+};
