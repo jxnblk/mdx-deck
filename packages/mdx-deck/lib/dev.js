d5c15094fe83478f1389062ad8a67a947561e846
anotherjsguy <kuldeepkeshwar@gmail.com>
2019-06-05 08:34:48 +0530

    Update dev.js

--- packages/mdx-deck/lib/dev.js
+++ packages/mdx-deck/lib/dev.js
@@ -7,6 +7,7 @@ const serveStatic = require('serve-static')
 const devMiddleware = require('webpack-dev-middleware')
 const hotMiddleware = require('webpack-hot-middleware')
 const createConfig = require('./config')
+const WebSocket  = require('ws');
 
 module.exports = async (opts = {}) => {
   const config = createConfig(opts)
@@ -35,6 +36,26 @@ module.exports = async (opts = {}) => {
 
   return new Promise(resolve => {
     const server = http.createServer(app)
+    const wss = new WebSocket.Server({ server });
+    wss.on('connection', (ws) => {
+      ws.on('message', (data) => {
+        try{
+          const message= JSON.parse(data);
+          console.log('received: ', message);
+          if(message.type==='broadcast'){
+            wss.clients
+            .forEach(client => {
+                if (client != ws) {
+                    client.send(JSON.stringify(message));
+                }    
+            });
+          }
+        }catch(err){
+          console.error('error in parsing message ',err)
+        }
+        
+      });
+    });
     compiler.hooks.done.tap('mdx-deck', () => {
       resolve(server)
     })
