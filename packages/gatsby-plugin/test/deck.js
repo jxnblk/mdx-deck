65aaf342c54fd6999143daba227b27f52ef144f4
Brent Jackson <jxnblk@gmail.com>
2020-02-01 12:04:42 -0500

    Adjust tests

---
+++ packages/gatsby-plugin/test/deck.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+  render,
+  fireEvent,
+  cleanup
+} from '@testing-library/react'
+import Deck from '../src/deck'
+
+afterEach(cleanup)
+
+test('renders', () => {
+})
