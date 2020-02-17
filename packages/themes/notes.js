ca146204b8f156d43237e46f62843da216872a3f
zyhou <richard.maxime9@gmail.com>
2020-02-17 18:33:14 +0100

    fix color on notes themes

--- packages/themes/notes.js
+++ packages/themes/notes.js
@@ -1,8 +1,15 @@
+const white = '#fff'
+const black = '#000'
+
 export default {
   googleFont:
     'https://fonts.googleapis.com/css?family=Annie+Use+Your+Telescope',
   fonts: {
     body: '"Annie Use Your Telescope", cursive',
+  },
+  colors: {
+    text: black,
+    background: white,
   },
   styles: {
     root: {
