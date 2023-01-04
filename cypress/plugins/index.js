c7843d65dcb0dd43148b3fb3437e2e5089df55c3
Jason Laster <jason.laster.11@gmail.com>
2023-01-03 17:58:31 -0800

    Install replay plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+const cypressReplay = require('@replayio/cypress')
+
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -14,4 +16,6 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  cypressReplay.default(on, config)
+  return config
 }
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,5 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+require('@replayio/cypress/support')
